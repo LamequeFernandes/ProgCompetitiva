6fe94835100676047be1fc781eacea3ada4a92f8
Lameque Fernandes <lamequesao@gmail.com>
2021-08-18 21:18:44 -0300

    mashup3 questaoD

---
+++ Mashup3/moedasInfinitas.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+
+   int a, n; cin >> a >> n;
+
+   if ( n * 500 > a )
+      cout << "Sim" << endl;
+   else
+      cout << "Nao" << endl;
+
+   return 0;
+}
